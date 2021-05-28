202fca73db37c067046e5e02c06b81426874325d
Anthony Garcia Jr <potentshull@gmail.co>
2021-05-28 11:55:29 -0400

    refactoring styling using bulma framework

--- JS/scoreKeeper/score.js
+++ JS/scoreKeeper/score.js
@@ -4,18 +4,18 @@ const scoreP1 = document.querySelector('#score-p1'); // P1 Score
 const scoreP2 = document.querySelector('#score-p2'); // P2 Score 
 const playTo = document.querySelector('#playto'); // Score to win selection
 const reset = document.querySelector('#btn-reset'); // Reset button
-let goal = playTo.options[playTo.selectedIndex].value; // Score to win
+let winningScore = playTo.options[playTo.selectedIndex].value; // Score to win
 
 // Update score to win when selection changes
 playTo.addEventListener('change', () => {
-   goal = playTo.options[playTo.selectedIndex].value;
+   winningScore = playTo.options[playTo.selectedIndex].value;
 })
 
 // Update P1 score when button is clicked
 btnP1.addEventListener('click', () => {
    scoreP1.innerText++;
    // Disable buttons when score is reached
-   if (scoreP1.innerText == goal) {
+   if (scoreP1.innerText == winningScore) {
       btnP1.setAttribute('disabled', true);
       btnP2.setAttribute('disabled', true);
       scoreP1.style.color = 'green';
@@ -27,7 +27,7 @@ btnP1.addEventListener('click', () => {
 btnP2.addEventListener('click', () => {
    scoreP2.innerText++;
    // Disable buttons when score is reached
-   if (scoreP2.innerText == goal) {
+   if (scoreP2.innerText == winningScore) {
       btnP1.setAttribute('disabled', true);
       btnP2.setAttribute('disabled', true);
       scoreP1.style.color = 'red';
